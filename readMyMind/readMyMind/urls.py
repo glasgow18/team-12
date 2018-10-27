911796ba6cf8f8173009c8c53aefd356f1fe13e1
heldtPawel <heldt.pawel@gmail.com>
2018-10-27 20:46:16 +0100

    app started #WowSoProductive

--- readMyMind/readMyMind/urls.py
+++ readMyMind/readMyMind/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from chatbot import views
+
 
 urlpatterns = [
+    path('', include('chatbot.urls')),
+    path('chatbot/', include('chatbot.urls')),
     path('admin/', admin.site.urls),
 ]
